e72ea094d2ef230601fff198f7d75dd084815480
Lucia Pereyra <lumes77iza@gmail.com>
2021-07-31 23:19:22 -0300

    fixed url button

--- frontend/src/pages/rpsls/componentsRpsls/GiveUpButton.jsx
+++ frontend/src/pages/rpsls/componentsRpsls/GiveUpButton.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import '../css/indexRpsls.css'
-
-function GiveUpButton({giveUp}){
-    async function backAndGiveUp(){
-        giveUp();
-        alert("Usted se rendira y volvera al lobby");
-        window.location.href = "http://localhost:3000/" //redirect to the lobby
-    }
-    return (
-        <div>
-            <button className="button"onClick={backAndGiveUp}>Rendirse</button>
-        </div>
-    )
+import React from "react";
+import "../css/indexRpsls.css";
+import { URL_FRONT as BASE_URL } from "../../../common/constants";
+function GiveUpButton({ giveUp }) {
+  async function backAndGiveUp() {
+    giveUp();
+    alert("Usted se rendira y volvera al lobby");
+    window.location.href = `${BASE_URL}`; //redirect to the lobby
+  }
+  return (
+    <div>
+      <button className="button" onClick={backAndGiveUp}>
+        Rendirse
+      </button>
+    </div>
+  );
 }
 export default GiveUpButton;
