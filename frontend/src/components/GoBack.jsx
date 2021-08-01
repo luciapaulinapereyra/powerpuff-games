f41e074f339195e1bcdc5602708b783a6592e57f
Lucia Pereyra <lumes77iza@gmail.com>
2021-07-31 23:11:39 -0300

    fixed frontend url

--- frontend/src/components/GoBack.jsx
+++ frontend/src/components/GoBack.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-
-function GoBack(props){
-    async function back(){
-        window.location.href = "http://localhost:3000/"
-    }
-    return (
-        <div>
-            <button className={props.css} onClick={back}>Volver al Lobby</button>
-        </div>
-    )
+import React from "react";
+import { URL_FRONT as BASE_URL } from "../common/constants";
+function GoBack(props) {
+  async function back() {
+    window.location.href = `${BASE_URL}`;
+  }
+  return (
+    <div>
+      <button className={props.css} onClick={back}>
+        Volver al Lobby
+      </button>
+    </div>
+  );
 }
 export default GoBack;
